1a0b22f4018e19cb1df504dfc71a45f23288ec2f
AdarshGrewal <adarsh@aosip.dev>
2021-06-30 22:54:22 +0530

    sweet: add prebuilt GcamGo
    Change-Id: I15e7df9bc64dc32f26a570af09fe4e885f35ecea
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_sweet/+/5260
    Reviewed-by: Adarsh Grewal <adarsh@aosip.dev>
    Tested-by: Adarsh Grewal <adarsh@aosip.dev>

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    GCamGo
+
 # Device Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
